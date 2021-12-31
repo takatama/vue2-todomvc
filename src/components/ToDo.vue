1318ca92eb434492eefb98b00e5f3219edcc1987
takatama <takatama@users.noreply.github.com>
2021-12-31 21:12:01 +0900

    added addTodo()

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -1,10 +1,23 @@
 <template>
-  <ul class="todo-list"></ul>
+  <ul class="todo-list">
+    <li class="todo" v-for="todo in todos" :key="todo.id">{{ todo.title }}</li>
+  </ul>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    addTodo(title) {
+      this.todos.push({
+        id: this.todos.length, title, completed: false
+      })
+    }
+  },
+  data() {
+    return {
+      todos: []
+    }
+  }
 }
 </script>
 
--- tests/unit/ToDo.spec.js
+++ tests/unit/ToDo.spec.js
@@ -15,4 +15,14 @@ describe('ToDo', () => {
   it('renders a todo-list', () => {
     expect(wrapper.find('.todo-list').exists()).toBe(true)
   })
+
+  it('renders todos after calling addTodo()', async () => {
+    await wrapper.vm.addTodo('1st task')
+    const todos1 = wrapper.findAll('.todo')
+    expect(todos1.length).toBe(1)
+
+    await wrapper.vm.addTodo('2nd task')
+    const todos2 = wrapper.findAll('.todo')
+    expect(todos2.length).toBe(2)
+  })
 })
